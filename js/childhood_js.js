16f96619d85643ca746a5fef21cce4f5939b78aa
Sarah Chaloult <chaloult@umich.edu>
2025-04-16 03:41:33 -0400

    added tab functionality to card flipping

--- js/childhood_js.js
+++ js/childhood_js.js
@@ -1,8 +1,10 @@
 const game_cards = document.querySelectorAll('.flip_card');
-
 for (let i = 0; i < game_cards.length; i++) {
     game_cards[i].addEventListener('click', function () {
         this.classList.toggle('flipped');
-        console.log('flip triggered');
     });
+
+    game_cards[i].addEventListener('focus', function() {
+        this.classList.toggle('flipped');
+    })
 }
